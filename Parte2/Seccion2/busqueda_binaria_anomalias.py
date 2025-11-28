c0a3f60c1a02ef6007b49acc025f7ad3934b748d
BrandoMatute24 <brmatutelo@uide.edu.ec>
2025-11-28 15:24:50 +0000

    cambios13.1

--- Parte2/Seccion2/busqueda_binaria_anomalias.py
+++ Parte2/Seccion2/busqueda_binaria_anomalias.py
@@ -3,18 +3,22 @@
 def busqueda_binaria(lista, objetivo):
     """
     Realiza una búsqueda binaria en una lista ordenada.
-    """
 
+    Parámetros:
+    - lista: Lista de elementos ordenados.
+    - objetivo: Elemento a buscar.
+
+    Retorna:
+    - Índice del objetivo si se encuentra en la lista; -1 si no está presente.
+    """
     izquierda = 0
     derecha = len(lista) - 1
 
-    # ANOMALÍA 1: Variable definida pero nunca utilizada
     valor_innecesario = "no se usa"
 
     while izquierda <= derecha:
         medio = (izquierda + derecha) // 2
 
-        # ANOMALÍA 2: Uso de variable antes de ser inicializada
         if variable_sin_inicializar > 0:
             izquierda = medio + 1
 
